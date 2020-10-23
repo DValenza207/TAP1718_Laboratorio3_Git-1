604436edbcef2dd7698c08098a1a9f22bfc22521
Damiano <damianovalenza@gmail.com>
2020-10-23 12:30:57 +0200

    modificato

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -7,6 +7,7 @@ namespace TAP1718_Laboratorio3_Git
         public static void Main(string[] args)
         {
             Console.WriteLine("Se leggete questo messaggio, significa che siete tornati indietro nel tempo!");
+            Console.WriteLine("non capisco perchè dopo il reset sia rimasto tutto uguale e non è tornato tutto alla versione precedente");
         }
     }
 }
